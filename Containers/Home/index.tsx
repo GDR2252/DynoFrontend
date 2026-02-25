350c0fcc95fa8a945e8bc1474359bcde8e95207f
Dharmik Godhani <dharmikgodhani071@gmail.com>
2026-02-25 18:21:17 +0530

    Improved theme

--- Containers/Home/index.tsx
+++ Containers/Home/index.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from "react";
 import HomeFooter from "@/Components/Layout/HomeFooter";
 import HomeHeader from "@/Components/Layout/HomeHeader";
 import ScrollToTopButton from "@/Components/Layout/ScrollToTopButton";
+import { theme } from "@/styles/theme";
 
 interface HomeLayoutProps {
   children: ReactNode;
@@ -16,7 +17,7 @@ export default function HomeLayout({ children }: HomeLayoutProps) {
         minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
-        bgcolor: "background.default",
+        bgcolor: theme.palette.common.white,
       }}
     >
       <HomeHeader />
